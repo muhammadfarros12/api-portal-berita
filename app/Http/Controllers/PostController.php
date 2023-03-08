071ada91907df14c8660f3f3036b09a93840e8b5
ammaar <muhammadfarroos12@gmail.com>
2023-03-08 14:50:01 +0700

    show id user (author) as writer

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,6 +16,10 @@ class PostController extends Controller
     }
 
     public function show($id){
+        $post = Post::with('writer:id,username')->findOrFail($id);
+        return new PostDetailResource($post);
+    }
+    public function show2($id){
         $post = Post::findOrFail($id);
         return new PostDetailResource($post);
     }
